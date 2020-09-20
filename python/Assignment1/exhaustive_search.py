6519d30264d630541cae19b746920f5c49f6a8be
Alex-Lewandowski <aflewandowski@alaska.edu>
2020-09-19 16:23:11 -0800

    power_set to power_set_indicies, add bridges_share_city

--- python/Assignment1/exhaustive_search.py
+++ python/Assignment1/exhaustive_search.py
@@ -1,20 +1,26 @@
-
-
 def bridges_intersect(b1: list, b2: list) -> bool:
     return (b2[0] > b1[0] and b2[1] < b1[1]) or \
            (b2[0] < b1[0] and b2[1] > b1[1])
 
 
-def power_set(my_list):
-    p_set = list()
-    power_set_len = 1 << len(my_list) # 2^n
-    for counter in range(0, power_set_len):
+def bridges_share_city(bridges) -> bool:
+    cities_w = [item[0] for item in bridges]
+    cities_e = [item[1] for item in bridges]
+    return len(cities_e) != len(set(cities_e)) or \
+           len(cities_w) != len(set(cities_w))
+
+
+def power_set_indices(list_len):
+    p_set = []
+    power_set_len = 1 << list_len  # 2^n
+    for i in range(1, power_set_len):
         subset = []
-        for j in range(0, len(my_list)):
-            # Check if jth bit in the counter is set with a
-            # bitwise and between counter and 2**j.
-            if (counter & (1 << j)) > 0:
-                subset.append(my_list[j])
+        for j in range(0, list_len):
+            # Check if jth bit in the i is set with a
+            # bitwise AND between counter and 2^j.
+            if (i & (1 << j)) > 0:
+                print(f"{i}, {j}: {bin(j)[2:]}")
+                subset.append(j)
             if subset and subset not in p_set:
                 p_set.append(subset)
     return p_set
@@ -30,5 +36,16 @@ ex2 = [[0, 1, 3],
        [2, 0, 8],
        [2, 2, 6]]
 
-print(power_set([i for i, _ in enumerate(ex2)]))
-print(len(power_set([i for i, _ in enumerate(ex2)])))
+ex3 = [[0, 0, 3],
+       [1, 1, 5],
+       [2, 2, 4],
+       [3, 3, 8],
+       [4, 4, 6]]
+
+pwr_set = power_set_indices(len(ex2))
+print(pwr_set)
+print(len(pwr_set))
+
+print(bridges_share_city(ex2))
+print(bridges_share_city(ex3))
+
