a31cb8b5949a8db9c7b468f175c6d6b20a66ba7b
Alex-Lewandowski <aflewandowski@alaska.edu>
2020-09-20 18:08:12 -0800

    use lookup table to find bridge intersections

--- cpp/exhaustive_search/build.cpp
+++ cpp/exhaustive_search/build.cpp
@@ -1,5 +1,8 @@
-//
-// Created by alex on 9/20/20.
+// build.cpp
+// Alex Lewandowski
+// 9/20/20
+// CS F411
+// Chris Hartman
 //
 
 #include "build.h"
@@ -13,8 +16,35 @@ using std::set;
 #include <algorithm>
 using std::find;
 using std::max_element;
+#include <map>
+using std::map;
+using std::find;
+#include <utility>
+using std::pair;
+using std::make_pair;
+
 
 using Bridge = vector<int>;
+
+
+template <class T>
+set<T> vector_to_set(const vector<T> & vec) {
+    set<T> s;
+    for(auto i : vec){
+        s.insert(i);
+    }
+    return s;
+}
+
+
+template <class T>
+vector<T> subset_from_indices(const vector<int> & indices, const vector<T> & tees) {
+    auto subset = vector<T>(size(indices));
+    for(auto i=0; i<size(indices); ++i) {
+        subset[i] = tees[indices[i]];
+    }
+    return subset;
+}
 
 
 bool two_bridges_intersect(const Bridge & b1,
@@ -24,6 +54,23 @@ bool two_bridges_intersect(const Bridge & b1,
 }
 
 
+bool any_bridges_intersect(const vector<Bridge> & bridges, map<pair<Bridge, Bridge>, bool> bridge_x_table) {
+    if(size(bridges) > 1) {
+        for (auto i = 0; i < size(bridges)-1; ++i) {
+            for (auto j = 1; j < size(bridges); ++j) {
+                auto b_pair = make_pair(bridges[i], bridges[j]);
+                auto d_pair = make_pair(bridges[j], bridges[i]);
+                if(bridge_x_table[b_pair] || bridge_x_table[d_pair]){
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+
+/**
 bool any_bridges_intersect(const vector<Bridge> & bridges) {
     if(size(bridges) > 1) {
         for (auto i = 0; i < size(bridges)-1; ++i) {
@@ -36,17 +83,7 @@ bool any_bridges_intersect(const vector<Bridge> & bridges) {
     }
     return false;
 }
-
-
-template <class T>
-set<T> vector_to_set(const vector<T> & vec) {
-    set<T> s;
-    for(auto i : vec){
-        s.insert(i);
-    }
-    return s;
-}
-
+**/
 
 bool any_bridges_share_city(const vector<int> & cities_w,
                             const vector<int> & cities_e) {
@@ -75,18 +112,28 @@ vector<vector<int>> power_set_indices(int n) {
     return pwr_set;
 }
 
-
-template <class T>
-vector<T> subset_from_indices(const vector<int> & indices, const vector<T> & tees) {
-    auto subset = vector<T>(size(indices));
-    for(auto i=0; i<size(indices); ++i) {
-        subset[i] = tees[indices[i]];
+map<pair<Bridge, Bridge>, bool> make_bridge_x_lookup_table(const vector<Bridge> & bridges){
+    auto table = map<pair<Bridge, Bridge>, bool>();
+    for (auto i=0; i<size(bridges)-1; ++i){
+        for(auto j=1; j<size(bridges); ++j){
+            auto b_pair = make_pair(bridges[i], bridges[j]);
+            auto d_pair = make_pair(bridges[j], bridges[i]);
+            if(table.find(b_pair) == table.end() || table.find(d_pair) == table.end()){
+                table.insert({b_pair, two_bridges_intersect(b_pair.first, b_pair.second)});
+            }
+        }
     }
-    return subset;
+    return table;
 }
 
-
 int build(int w, int e, const vector<Bridge> & bridges){
+    if (size(bridges) < 1){
+        return 0;
+    }
+    if(size(bridges) < 2){
+        return bridges[0][2];
+    }
+    const auto bridge_x_table = make_bridge_x_lookup_table(bridges);
     auto cities_w = vector<int>{};
     auto cities_e = vector<int>{};
     auto tolls = vector<int>{};
@@ -96,20 +143,20 @@ int build(int w, int e, const vector<Bridge> & bridges){
         cities_e.push_back(bridges[i][1]);
         tolls.push_back(bridges[i][2]);
     }
+    if(!(any_bridges_intersect(bridges, bridge_x_table) || any_bridges_share_city(cities_e, cities_w))){
+        return accumulate(tolls.begin(), tolls.end(), 0);
+    }
     auto pwr_set_indices = power_set_indices(size(bridges));
     auto toll_totals = vector<int>{};
     for(const auto& i : pwr_set_indices) {
         vector<Bridge> subset = subset_from_indices(i, bridges);
-        if(!(any_bridges_intersect(subset) or
+        if(!(any_bridges_intersect(subset, bridge_x_table) or
              any_bridges_share_city(subset_from_indices(i, cities_w),
                                     subset_from_indices(i, cities_e)))) {
             auto tolls_sub = subset_from_indices(i, tolls);
             toll_sum = accumulate(tolls_sub.begin(), tolls_sub.end(), 0);
             toll_totals.push_back(toll_sum);
         }
-    }
-    if(size(toll_totals) < 1){
-        return 0;
     }
     return *max_element(toll_totals.begin(), toll_totals.end());
 }
--- cpp/exhaustive_search/build.h
+++ cpp/exhaustive_search/build.h
@@ -1,13 +1,36 @@
+// build.h
+// Alex Lewandowski
+// 9/20/20
+// CS F411
+// Chris Hartman
 //
-// Created by alex on 9/20/20.
-//
+
 #include <vector>
+#include <set>
 
 #ifndef EXHAUSTIVE_SEARCH_BUILD_H
 #define EXHAUSTIVE_SEARCH_BUILD_H
 
 using Bridge = std::vector<int>;
 
+template <class T>
+std::set<T> vector_to_set(const std::vector<T> & vec);
+
+template <class T>
+std::vector<T> subset_from_indices(const std::vector<int> & indices,
+                                   const std::vector<T> & tees);
+
+bool two_bridges_intersect(const Bridge & b1,
+                           const Bridge & b2);
+
+bool any_bridges_intersect(const std::vector<Bridge> & bridges);
+
+bool any_bridges_share_city(const std::vector<int> & cities_w,
+                            const std::vector<int> & cities_e);
+
+std::vector<std::vector<int>> power_set_indices(int n);
+
 int build(int w, int e, const std::vector<Bridge> & bridges);
+
 
 #endif//EXHAUSTIVE_SEARCH_BUILD_H
